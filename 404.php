457b2d944aab4fcd18bfee2e6d9f163cc19f08e2
David Endersby <daendersby@gmail.com>
2015-03-24 19:14:33 +0000

    Added propper 404 page, sorted music page font, added negitive margin on blog

--- 404.php
+++ 404.php
@@ -1,12 +1,17 @@
 <?php get_header(); ?>
+<header class="container-fluid frontPage-banner blue-bg">
+  <div class="container">
+  <div class="row">
+    <div class="col-md-12 banner-text">
+      <h1><span>Oh No!</span><br>
+      404 - Page Not Found</h1>
+      <h2>Maybe its got lost. Can you find it?</h2>
+      <p>Start searching at the <a href="<?php echo get_home_url();?>">Homepage</a></p>
+    </div>
+  </div>
+</header>
 
-<h1>This is 404.php</h1>
 
-
-<?php
-$paged = htmlspecialchars($_GET["paged"]);
-echo $paged;
-?>
 
 
 <?php get_footer(); ?>
