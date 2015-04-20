1b6b71b0db4bb92b7f659e8f1c8ad682a27b8939
David Endersby <daendersby@gmail.com>
2015-04-20 19:21:28 +0100

    Made about page responsive

--- about.php
+++ about.php
@@ -3,27 +3,26 @@
 ?>
 <?php get_header(); ?>
 
-<header class="container-fluid">
+<header class="container">
 
-  <div class="col-md-12">
-    <h1>About</h1>
-    <p>A little bit about me</p>
-  </div>
-
-</header>
-
-<main class="container content-wrapper">
   <section class="col-md-12 about-intro">
     <div class="card">
-      <h2>TL;DR</h2>
+      <div class="about-intro-title">
+        <h1>About</h1>
+        <p>A little bit about me</p>
+      </div>
       <p>I'm a 24 year old, London based web design/developer currently working for Universal Music. I've got absolutally no formal qualifications in what I do and I'm completely self taught, having spent many evenings and weekends working my way through Treehouse. I do absolutally love Web development though. I'm a propper little (okay not so little) Nerd. It wasn't always this way. The original plan was to get a job in the geography world, probably involving me standing out in the middle of a field, in the rain, taking soil samples. Then I caught the web dev bug and within 2 years was working for the biggest music companies in the world. It hasn't been plain sailing but its totally worth it and I'm not stopping here. I'm currently spending my evenings and weekends learning new skills like Angular and Node - Yes the guys and girls at work think its wierd 'working' at the weekends, but I'm not too bothered.</p>
     </div>
   </section>
 
-
+</header>
+<main class="container content-wrapper">
   <section class="col-md-4 about-profile">
     <div class="card">
-      <img src="https://graph.facebook.com/10152555016045907/picture?type=large" itemprop="image">
+      <div class="col-sm-4 col-md-12">
+        <img src="https://graph.facebook.com/10152555016045907/picture?type=large" itemprop="image"/>
+      </div>
+      <div class="col-sm-8 col-md-12">
       <h2>David Endersby</h2>
       <ul class="about-profile-intro">
         <li>
@@ -39,21 +38,26 @@
           <span>Plymouth University</span></p>
         </li>
       </ul>
-      <h3>Skills</h3>
-      <ul class="about-profile-skills">
-        <li class="about-profile-html"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-html.svg" alt="HTML5 Logo"></li>
-        <li class="about-profile-css"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-css.svg" alt="CSS3 Logo"></li>
-        <li class="about-profile-sass"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-sass.svg" alt="Sass Logo"></li>
-        <li class="about-profile-js"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-js.svg" alt="Javascript Logo"></li>
-        <li class="about-profile-php"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-php.svg" alt="PHP Logo"></li>
-        <li class="about-profile-wp"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-wp.svg" alt="Wordpress Logo"></li>
-      </ul>
-      <h3>Also interested in</h3>
-      <ul>
-        <li>Angular.js</li>
-        <li>Node.js</li>
-        <li>Security &amp; Encryption</li>
-      </ul>
+      </div>
+      <div class="about-profile-skills">
+        <h3>Skills</h3>
+        <ul>
+          <li class="about-profile-html"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-html.svg" alt="HTML5 Logo"></li>
+          <li class="about-profile-css"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-css.svg" alt="CSS3 Logo"></li>
+          <li class="about-profile-sass"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-sass.svg" alt="Sass Logo"></li>
+          <li class="about-profile-js"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-js.svg" alt="Javascript Logo"></li>
+          <li class="about-profile-php"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-php.svg" alt="PHP Logo"></li>
+          <li class="about-profile-wp"><img src="<?php bloginfo('template_directory'); ?>/img/about/logos-wp.svg" alt="Wordpress Logo"></li>
+        </ul>
+      </div>
+      <div>
+        <h3>Also interested in</h3>
+        <ul>
+          <li>Angular.js</li>
+          <li>Node.js</li>
+          <li>Security &amp; Encryption</li>
+        </ul>
+      </div>
     </div>
   </section>
 
@@ -73,7 +77,7 @@
     <div class="card">
       <h2>Battlefield 4</h2>
       <div class="col-md-12 about-battlefield-summary">
-        <img class="col-xs-4" src="http://www.bf4stats.com/img/bf4/ranks/r111.png" alt="Rank 111 - Brigadier General II emblem">
+        <img class="col-sm-4" src="http://www.bf4stats.com/img/bf4/ranks/r111.png" alt="Rank 111 - Brigadier General II emblem">
         <div class="col-sm-7 about-battlefield-summary">
           <a href="http://battlelog.battlefield.com/bf4/user/Davetherave2014" id="battlefieldUserName">Davetherave2010</a>
           <p id="battlefieldRank">Brigadier General II</p>
