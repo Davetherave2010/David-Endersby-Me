45f35dd9e657de7a319eb4f4e81ccc019a450537
David Endersby <daendersby@gmail.com>
2015-05-24 15:06:31 +0100

    Fixed lack of svg images on chrome

--- front-page.php
+++ front-page.php
@@ -38,16 +38,17 @@
       <h2>Blog</h2>
       <p>My blog is a space for me to talk about interesting things that are going on in my life and things that I'm interested in around the world - like news and events. It contains some of my experiences and what I have been up to, for example: Web conferences, and what I've learnt from them.Its alto a place where I post relese notes for projects that I've worked on, showing new features and fixed bugs. Its all there.</p>
       <a href="<?php the_permalink('blog'); ?>">Read my Blog &rarr;</a>
-    </section>        
-    <svg class="frontPage-icons col-md-4 hidden-xs hidden-sm" width="100%" height="280">
-      <image xlink:href="<?php bloginfo('template_directory'); ?>/img/blog.svg" src="<?php bloginfo('template_directory'); ?>/img/blog.png" width="100%" height="280" class="img-responsive" alt="An illustration of a red megaphone in a hand. It looks like it is being shouted through"/>
-    </svg>
+    </section>
+    <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/blog.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/blog.png'" alt="An illustration of a red megaphone in a hand. It looks like it is being shouted through">
   </div>
 
   <div class="row">
-    <svg class="col-md-4 hidden-xs hidden-sm frontPage-icons" width="100%" height="280">
+    <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/about1.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/about1.png'" alt="An illustrated man holding an xray of his abdomen up to his chest">
+
+<!--     <svg class="col-md-4 hidden-xs hidden-sm frontPage-icons" width="100%" height="280">
       <image xlink:href="<?php bloginfo('template_directory'); ?>/img/about1.svg" src="<?php bloginfo('template_directory'); ?>/img/about1.png" width="100%" height="280" class="img-responsive" alt="An illustrated man holding an xray of his abdomen up to his chest"/>
     </svg>
+ -->    
     <section class="col-md-8 padding-right-8pc">
       <h2>About Me</h2>
       <p>The about page shows some of my achivements using data gathered from the furthest coners of the internet. It shows my Treehouse score and what I'm currently learning as well as how much time I've spent (or wasted) on Battlefield. Top 5% worldwide though, Just saying.</p>
