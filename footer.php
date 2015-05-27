599dc5625de1b95a4d8c450b27d4616277c507b0
David Endersby <daendersby@gmail.com>
2015-05-27 20:50:07 +0100

    Fixed twitter footer link

--- footer.php
+++ footer.php
@@ -3,19 +3,10 @@
 	      <div class='col-md-12'>
 					<!-- Icons from Ionicons-->
 	      	<a class="footer-icons facebook" href="//www.facebook.com/davidendersby" title="facebook" target="_blank">      
-<!-- 	      		<svg>
-			        <image xlink:href="<?php echo get_template_directory_uri();?>/img/social-facebook.svg" src="<?php echo get_template_directory_uri();?>/img/social-facebook.png" width="100%" height="100%" alt="The Facebook f icon"/>
-			      </svg> -->
 			    </a>
-			    <a class="footer-icons twitter" href="//www.twitter.co.uk/davidendersby1" title="twitter" target="_blank">
-<!-- 			      <svg>
-			        <image xlink:href="<?php echo get_template_directory_uri();?>/img/social-twitter.svg" src="<?php echo get_template_directory_uri();?>/img/social-twitter.png" width="100%" height="100%" alt="The Twitter bird icon"/>
-			      </svg> -->
+			    <a class="footer-icons twitter" href="//www.twitter.com/davidendersby1" title="twitter" target="_blank">
 	      	</a>
 	      	<a class="footer-icons github" href="//github.com/Davetherave2010/David-Endersby.me" title="github" target="_blank">
-<!-- 			      <svg>
-			        <image xlink:href="<?php echo get_template_directory_uri();?>/img/social-twitter.svg" src="<?php echo get_template_directory_uri();?>/img/social-twitter.png" width="100%" height="100%" alt="The Github icon"/>
-			      </svg> -->
 	      	</a>
 	      	<p>Website made with <span class="glyphicon glyphicon-heart"></span> in London, UK<br>
 	      		&copy; <?php echo date("Y");?> David Endersby</p>
