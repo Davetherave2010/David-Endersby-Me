78320f9eba73c4dc305cb49941465e05f6fe9b4a
David Endersby <daendersby@gmail.com>
2015-09-16 21:29:49 +0100

    Fixed blog and about links

--- front-page.php
+++ front-page.php
@@ -22,7 +22,7 @@
         <h2>David Endersby<wbr> MSc BSc(Hons) FGS</h2>
     </div>
     <div class="col-md-10 col-md-offset-1">
-        <p> Hello, I am a Web design developer based in london with a background in geography and geological hazards. I currently spend my daytime working for Universal Music UK and the rest of my time doing personal projects based around my web dev skill set. I'm actively persuing pure javascript and looking to learn Node and Angular to be able to build web applications.</p>
+        <p> Hello, I am a Web design developer based in London with a background in geography and geological hazards. I currently spend my daytime working for Universal Music UK and the rest of my time doing personal projects based around my web dev skill set. I'm actively persuing a mean javascript stack and looking to learn Node and Angular to be able to build web applications.</p>
 
 
         <p>Did I mention that I'm also a Fellow of the British Geological Society.</p>
@@ -36,8 +36,8 @@
   <div class="row">
     <section class="col-md-8 padding-left-8pc">
       <h2>Blog</h2>
-      <p>My blog is a space for me to talk about interesting things that are going on in my life and things that I'm interested in around the world - like news and events. It contains some of my experiences and what I have been up to, for example: Web conferences, and what I've learnt from them.Its alto a place where I post relese notes for projects that I've worked on, showing new features and fixed bugs. Its all there.</p>
-      <a href="<?php the_permalink('blog'); ?>">Read my Blog &rarr;</a>
+      <p>My blog is a space for me to talk about interesting things that are going on in my life and things that I'm interested in around the world - like news and events. It contains some of my experiences and what I have been up to, for example: Web conferences, and what I've learnt from them. Its alto a place where I post relese notes for projects that I've worked on, showing new features and fixed bugs. Its all there.</p>
+      <a href="<?php echo site_url( '/blog/'); ?>">Read my Blog &rarr;</a>
     </section>
     <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/blog.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/blog.png'" alt="An illustration of a red megaphone in a hand. It looks like it is being shouted through">
   </div>
@@ -52,7 +52,7 @@
     <section class="col-md-8 padding-right-8pc">
       <h2>About Me</h2>
       <p>The about page shows some of my achivements using data gathered from the furthest coners of the internet. It shows my Treehouse score and what I'm currently learning as well as how much time I've spent (or wasted) on Battlefield. Top 5% worldwide though, Just saying.</p>
-      <a href="about/">Find out more &rarr;</a>
+      <a href="<?php echo site_url( '/about/'); ?>">Find out more &rarr;</a>
     </section>
   </div>
 </div>
