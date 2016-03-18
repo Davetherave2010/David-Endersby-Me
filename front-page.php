9db72e3113d9dfc28519937c5a6d69240743a597
David Endersby <daendersby@gmail.com>
2016-03-18 20:14:08 +0100

    Removed static content from homepage and made it dynamic

--- front-page.php
+++ front-page.php
@@ -16,44 +16,47 @@
   </div>
 </header>
 
-<div class="full-width row">
-  <section class="container introduction">
-    <div class="col-md-12 text-center">
-        <h2>David Endersby<wbr> MSc BSc(Hons) FGS</h2>
-    </div>
-    <div class="col-md-10 col-md-offset-1">
-        <p> Hello, I am a Web design developer based in London with a background in geography and geological hazards. I currently spend my daytime working for Universal Music UK and the rest of my time doing personal projects based around my web dev skill set. I'm actively persuing a mean javascript stack and looking to learn Node and Angular to be able to build web applications.</p>
 
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-        <p>Did I mention that I'm also a Fellow of the British Geological Society.</p>
-    </div>
-  </section>
-  <hr>
-</div>
-
-<div class="container mainContent content-wrapper">
-
-  <div class="row">
-    <section class="col-md-8 padding-left-8pc">
-      <h2>Blog</h2>
-      <p>My blog is a space for me to talk about interesting things that are going on in my life and things that I'm interested in around the world - like news and events. It contains some of my experiences and what I have been up to, for example: Web conferences, and what I've learnt from them. Its alto a place where I post relese notes for projects that I've worked on, showing new features and fixed bugs. Its all there.</p>
-      <a href="<?php echo site_url( '/blog/'); ?>">Read my Blog &rarr;</a>
+  <div class="full-width row">
+    <section class="container introduction">
+      <div class="col-md-12 text-center">
+          <h2>David Endersby<wbr> MSc BSc(Hons) FGS</h2>
+      </div>
+      <div class="col-md-10 col-md-offset-1">
+          <?php echo the_content(); ?>
+      </div>
     </section>
-    <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/blog.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/blog.png'" alt="An illustration of a red megaphone in a hand. It looks like it is being shouted through">
+    <hr>
   </div>
 
-  <div class="row">
-    <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/about1.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/about1.png'" alt="An illustrated man holding an xray of his abdomen up to his chest">
+  <div class="container mainContent content-wrapper">
 
-<!--     <svg class="col-md-4 hidden-xs hidden-sm frontPage-icons" width="100%" height="280">
-      <image xlink:href="<?php bloginfo('template_directory'); ?>/img/about1.svg" src="<?php bloginfo('template_directory'); ?>/img/about1.png" width="100%" height="280" class="img-responsive" alt="An illustrated man holding an xray of his abdomen up to his chest"/>
-    </svg>
- -->    
-    <section class="col-md-8 padding-right-8pc">
-      <h2>About Me</h2>
-      <p>The about page shows some of my achivements using data gathered from the furthest coners of the internet. It shows my Treehouse score and what I'm currently learning as well as how much time I've spent (or wasted) on Battlefield. Top 5% worldwide though, Just saying.</p>
-      <a href="<?php echo site_url( '/about/'); ?>">Find out more &rarr;</a>
-    </section>
+    <div class="row">
+      <section class="col-md-8 padding-left-8pc">
+        <h2><?php the_field('blog_title'); ?></h2>
+        <p><?php the_field('blog_text'); ?></p>
+        <a href="<?php echo site_url( '/blog/'); ?>">Read my Blog &rarr;</a>
+      </section>
+      <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/blog.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/blog.png'" alt="An illustration of a red megaphone in a hand. It looks like it is being shouted through">
+    </div>
+
+    <div class="row">
+      <img class="frontPage-icons col-md-4 hidden-xs hidden-sm" src="<?php bloginfo('template_directory'); ?>/img/about1.svg" onerror="this.onerror=null; this.src='<?php bloginfo('template_directory'); ?>/img/about1.png'" alt="An illustrated man holding an xray of his abdomen up to his chest">
+
+  <!--     <svg class="col-md-4 hidden-xs hidden-sm frontPage-icons" width="100%" height="280">
+        <image xlink:href="<?php bloginfo('template_directory'); ?>/img/about1.svg" src="<?php bloginfo('template_directory'); ?>/img/about1.png" width="100%" height="280" class="img-responsive" alt="An illustrated man holding an xray of his abdomen up to his chest"/>
+      </svg>
+   -->    
+      <section class="col-md-8 padding-right-8pc">
+        <h2><?php the_field('about_title'); ?></h2>
+        <p><?php the_field('about_text'); ?></p>
+        <a href="<?php echo site_url( '/about/'); ?>">Find out more &rarr;</a>
+      </section>
+    </div>
   </div>
-</div>
+<?php endwhile; else : ?>
+  <p><?php _e( 'Sorry, no posts matched your criteria.' ); ?></p>
+<?php endif; ?>
 <?php get_footer(); ?>
